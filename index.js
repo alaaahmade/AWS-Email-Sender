3a48ccf0ed877566a76f983c471a5ff8b8c8dbac
alaaahmade <alaaahmade66@gmail.com>
2025-06-03 12:14:43 +0300

    new

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ app.post('/messages', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-  console.log(`Swagger docs at http://localhost:${PORT}/api-docs`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
+  console.log(`Swagger docs at http://0.0.0.0:${PORT}/api-docs`);
 });
